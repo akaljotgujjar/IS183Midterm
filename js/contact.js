a214079da06a50cf21db1ee727c757eac06a2c39
Akaljot Gujjar <akaljotgujjar@gmail.com>
2020-04-01 15:39:55 -0700

    finished exam

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+var Contact = /** @class */ (function () {
+    function Contact() {
+    }
+    Contact.prototype.goHome = function () {
+        window.location.href = 'index.html';
+    };
+    return Contact;
+}());
+var contact = new Contact();
